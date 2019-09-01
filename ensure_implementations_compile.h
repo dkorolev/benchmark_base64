1ecde8e4159c43c7fe55224a827b521cf9ea1aaa
Dima Korolev <dmitry.korolev@gmail.com>
2019-08-31 19:54:56 -0700

    Header paths fix.

--- ensure_implementations_compile.h
+++ ensure_implementations_compile.h
@@ -2,13 +2,13 @@
 
 // This dummy header file is just to make sure code completion works fine for the `implemntations.h` file.
 
-#include "../current/bricks/dflags/dflags.h"
-#include "../current/bricks/strings/chunk.h"
-#include "../current/bricks/strings/join.h"
-#include "../current/bricks/time/chrono.h"
-#include "../current/bricks/util/base64.h"
-#include "../current/bricks/util/random.h"
-#include "../current/bricks/util/singleton.h"
+#include "current/bricks/dflags/dflags.h"
+#include "current/bricks/strings/chunk.h"
+#include "current/bricks/strings/join.h"
+#include "current/bricks/time/chrono.h"
+#include "current/bricks/util/base64.h"
+#include "current/bricks/util/random.h"
+#include "current/bricks/util/singleton.h"
 
 #ifndef IMPLEMENTATION
 #define IMPLEMENTATION(x) struct unused_implementation_##x
